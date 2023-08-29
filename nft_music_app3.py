4a46a2af9ce516c0c40e243abe05c5821dd4560b
DM <DM@Dan-Ms-MacBook-Air-M1.local>
2023-08-29 19:19:20 -0300

    adding new files

--- nft_music_app3.py
+++ nft_music_app3.py
@@ -1,21 +1,21 @@
 import streamlit as st
 from web3 import Web3
+import ipfshttpclient
 
-# Mock upload function
-def upload_to_cloud(file):
-    # Your logic to upload the file to the cloud
-    # For now, let's just return a mock URI.
-    return "http://your-cloud-storage-link-to-the-song"
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Connect to Ethereum
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 connected = w3.is_connected()
 
+# Connect to IPFS
+ipfs_client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')  # Assuming your IPFS daemon is running locally
+
 # Streamlit UI
 st.title("NFT Music App")
 
 if connected:
-    st.success("Connected to Ethereum")
+   st.success("Connected to Ethereum")
 else:
     st.error("Not connected to Ethereum. Please ensure your node/Ganache is running.")
 
@@ -25,8 +25,10 @@ uploaded_file = st.file_uploader("Choose a music file", type="mp3")
 if uploaded_file:
     st.audio(uploaded_file, format='audio/mp3')
     
-    # Get the URI from our mock upload function
-    song_uri = upload_to_cloud(uploaded_file)
+    # Upload to IPFS
+    ipfs_response = ipfs_client.add(uploaded_file)
+    song_ipfs_hash = ipfs_response['Hash']
+    song_uri = f"https://ipfs.io/ipfs/{song_ipfs_hash}"
     
     # Display the URI or use it in your logic
     st.write(f"Song URI: {song_uri}")
